cc053b37d9a4bd6a7a49a2361720ed3cf52194e7
Renan Matos <49045257+rjsmatos10@users.noreply.github.com>
2020-11-19 13:41:12 -0300

    Create TestAuthor.java

---
+++ TestAuthor.java
@@ -0,0 +1,10 @@
+//Nomes : Leandro Pereira dos Santos CB:3005372
+// Renan Josué Silva de Matos
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author carlos = new Author("Wellington Tuler","tulermoraes@yahoo.com", 'm');
+
+        System.out.println(carlos.toString());
+    }
+}
+ 
